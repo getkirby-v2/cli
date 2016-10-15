159ccb5281d197b6a21fd3c789f1e81b62774a27
Maximilian Stauss <max.stauss@gmail.com>
2016-10-15 16:32:49 +0200

    Proof of Concept: Show latest release in version
    Uses the Github API to show the latest remote version
    
    ```
    ~ kirby version
    Core:	2.3.2	(latest: 2.3.2)
    Toolkit:	2.3.2	(latest: 2.3.2)
    Panel:	2.3.2	(latest: 2.3.2)
    ```

--- src/Command/Version.php
+++ src/Command/Version.php
@@ -7,6 +7,7 @@ use Panel;
 use RuntimeException;
 use Kirby;
 use Kirby\Cli\Command;
+use GuzzleHttp\Client;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -14,9 +15,24 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class Version extends Command {
 
+  protected $client;
+
+  function __construct() {
+    parent::__construct();
+
+    $this->client = new Client([
+      'base_uri' => 'https://api.github.com'
+    ]);
+  }
+
   protected function configure() {
     $this->setName('version')
          ->setDescription('Prints the current versions of the core, the toolkit and the panel of your installation');
+  }
+
+  protected function getLatestRelease($repo) {
+    $response = $this->client->request('GET', 'repos/getkirby/' . $repo . '/tags');
+    return json_decode($response->getBody(), true)[0]['name'];
   }
 
   protected function execute(InputInterface $input, OutputInterface $output) {
@@ -28,8 +44,8 @@ class Version extends Command {
     // bootstrap the core
     $this->bootstrap();
 
-    $output->writeln("<info>Core:\t\t" . kirby::version() . "</info>");
-    $output->writeln("<info>Toolkit:\t" . toolkit::version() . "</info>");
+    $output->writeln("<info>Core:\t\t" . kirby::version() . "\t(latest: " . $this->getLatestRelease('kirby') . ")</info>");
+    $output->writeln("<info>Toolkit:\t" . toolkit::version() . "\t(latest: " . $this->getLatestRelease('toolkit') . ")</info>");
 
     // also check for the panel version, if it is installed
     if(is_dir($this->dir() . '/panel')) {
@@ -41,7 +57,7 @@ class Version extends Command {
       // bootstrap the panel
       require $this->dir() . '/panel/app/bootstrap.php';
 
-      $output->writeln("<info>Panel:\t\t" . panel::version() . "</info>");
+      $output->writeln("<info>Panel:\t\t" . panel::version() . "\t(latest: " . $this->getLatestRelease('panel') . ")</info>");
 
     }
 
